67371c113e4cc3a7ff19f4a6696b361abc049f29
Kian Majlessi <kianmajl@gmail.com>
2023-05-31 21:44:56 +0330

    add create question view

--- question/views.py
+++ question/views.py
@@ -5,4 +5,20 @@ from rest_framework.permissions import IsAuthenticated, IsAdminUser
 from .models import Question
 
 
-# Create your views here.
+class CreateQuestionView(APIView):
+    permission_classes = (IsAdminUser,)
+
+    def post(self, request, *args, **kwargs):
+        question_type = request.data.get('type')
+        question_text = request.data.get('question')
+        template_id = request.data.get('template_id')
+        template = QuestionnaireTemplate.objects.get(id=template_id)
+        question_position = request.data.get('number')
+        style = request.data.get('style')
+        rows = request.data.get('rows')
+        options = request.data.get('options')
+        question = Question.objects.create(template=template, question_text=question_text,
+                                           question_position=question_position, question_type=question_type,
+                                           style=style, rows=rows, options=options)
+        question.save()
+        return Response({'message': 'Question created successfully'}, status=status.HTTP_201_CREATED)
