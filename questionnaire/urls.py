fba57b143740a1e67fcc32a30eb385503f8c5a3d
Kian Majlessi <kianmajl@gmail.com>
2023-06-01 20:34:43 +0330

    the last commit :))

--- questionnaire/urls.py
+++ questionnaire/urls.py
@@ -6,5 +6,10 @@ urlpatterns = [
     path('questionnaire_template/<int:pk>/', QuestionnaireTemplateRetrieveUpdateDestroyView.as_view()),
     path('questionnaires/', QuestionnaireList.as_view()),
     path('questionnaires/<int:pk>/', QuestionnaireDetail.as_view()),
-    path('questionnaires/number_questions/<int:pk>/', GetNumberQuestions.as_view()),
+    path('questionnaires/number_questions/', GetNumberQuestions.as_view()),
+    path('submissions/get/', SubmissionQuestionnaireGet.as_view()),
+    path('submissions/create/', SubmissionQuestionnaireCreate.as_view()),
+    path('submissions/delete/', SubmissionQuestionnaireDelete.as_view()),
+    path('submissions/not-responded-users/', UserNotRespondedSubmission.as_view()),
+
 ]
--- questionnaire/views.py
+++ questionnaire/views.py
@@ -3,8 +3,6 @@ from rest_framework.permissions import IsAuthenticated, IsAdminUser
 from .serializers import QuestionnaireTemplateSerializer, QuestionnaireSerializer
 from .models import QuestionnaireTemplate, Questionnaire, Submission
 from django.shortcuts import get_object_or_404
-from rest_framework.response import Response
-from rest_framework.views import APIView
 from rest_framework import filters
 from rest_framework import status
 from rest_framework.views import APIView
@@ -42,20 +40,17 @@ class QuestionnaireList(generics.ListCreateAPIView):
     search_fields = ['employee__first_name', 'employee__last_name', 'deadline', 'template__template_name']
     filter_backends = (filters.SearchFilter,)
     serializer_class = QuestionnaireSerializer
-    permission_classes = (IsAdminUser, )
+    permission_classes = (IsAdminUser,)
 
     def get_queryset(self):
         if self.request.user.is_staff:
             return Questionnaire.objects.filter(template__creator=self.request.user)
         return self.request.user.users.all()
 
-    def perform_create(self, serializer):
-        serializer.save(creator=self.request.user)
-
 
 class QuestionnaireDetail(generics.RetrieveUpdateDestroyAPIView):
     serializer_class = QuestionnaireSerializer
-    permission_classes = (IsAdminUser, )
+    permission_classes = (IsAdminUser,)
 
     def get_queryset(self):
         return Questionnaire.objects.filter(creator=self.request.user)
@@ -63,33 +58,35 @@ class QuestionnaireDetail(generics.RetrieveUpdateDestroyAPIView):
     def delete(self, request, pk=None):
         instance = QuestionnaireTemplate.objects.filter(id=pk)
         instance.delete()
-        return Response(status=status.HTTP_204_NO_CONTENT)    
+        return Response(status=status.HTTP_204_NO_CONTENT)
 
-      
+
 class SubmissionQuestionnaireGet(APIView):
-    permission_classes = (IsAdminUser, )
+    permission_classes = (IsAdminUser,)
 
     def get(self, request, *args, **kwargs):
         questionnaire_id = get_object_or_404(Questionnaire, id=request.GET.get('id'))
         submissions = Submission.objects.filter(questionnaire=questionnaire_id).values()
 
-        return Response(context={'submissions': submissions}, status=status.HTTP_200_OK)
+        return Response({'submissions': submissions}, status=status.HTTP_200_OK)
 
 
 class SubmissionQuestionnaireCreate(APIView):
-    permission_classes = (IsAdminUser, )
+    permission_classes = (IsAuthenticated,)
 
     def post(self, request, *args, **kwargs):
         questionnaire_id = get_object_or_404(Questionnaire, id=request.data.get('questionnaire'))
         user_id = request.data.get('user')
         user = questionnaire_id.users.get(id=user_id)
-        submission = Submission.objects.create(questionnaire=questionnaire_id, user=user, answers=request.data.get('answers'))
+        submission = Submission.objects.create(questionnaire=questionnaire_id, user=user,
+                                               answers=request.data.get('answers'))
         submission.save()
-        return Response(context={'message': 'submission created successfully!'}, status=status.HTTP_201_CREATED)
+        return Response({'message': 'submission created successfully!', 'id': submission.id},
+                        status=status.HTTP_201_CREATED)
 
 
 class SubmissionQuestionnaireDelete(APIView):
-    permission_classes = (IsAdminUser, )
+    permission_classes = (IsAdminUser,)
 
     def delete(self, request, *args, **kwargs):
         questionnaire_id = get_object_or_404(Questionnaire, id=request.GET.get('questionnaire_id'))
@@ -97,23 +94,22 @@ class SubmissionQuestionnaireDelete(APIView):
         for submission in submissions:
             submission.delete()
 
-        return Response({"message": f"submissions for questionnaire with id:{questionnaire_id} deleted successfully!"}, status=status.HTTP_204_NO_CONTENT)
+        return Response({"message": f"submissions for questionnaire with id:{questionnaire_id} deleted successfully!"},
+                        status=status.HTTP_204_NO_CONTENT)
 
-class UserNotRespondedSubmisssion(APIView):
-    permission_classes = (IsAdminUser, )
+
+class UserNotRespondedSubmission(APIView):
+    permission_classes = (IsAdminUser,)
 
     def get(self, request, *args, **kwargs):
-        questionnaire_id = get_object_or_404(Questionnaire, id=request.GET.get('id'))
-        users = questionnaire_id.users.all()
-        submissions = Submission.objects.filter(questionnaire=questionnaire_id).values_list('user', flat=True)
-        users_not_responded = []
-        for user in users:
-            if user not in submissions:
-                users_not_responded.append(user)
-        return Response(context={'users_not_responded': users_not_responded}, status=status.HTTP_200_OK)
+        questionnaire = get_object_or_404(Questionnaire, id=request.GET.get('id'))
+        users = questionnaire.users.all().exclude(users__in=Submission.objects.filter(questionnaire=questionnaire).values().values_list('user', flat=True)).values()
+        return Response({'users_not_responded': users}, status=status.HTTP_200_OK)
 
 
 class GetNumberQuestions(APIView):
+    permission_classes = (IsAdminUser,)
 
     def get(self, request, *args, **kwargs):
-        return Response({"number_questions": Question.objects.filter(template__id=kwargs['pk']).count()})
+        questions = Question.objects.filter(template__id=request.GET.get('id')).values()
+        return Response({'number_questions': len(questions)}, status=status.HTTP_200_OK)
